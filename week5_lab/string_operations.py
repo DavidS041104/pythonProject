933534e956d799fd383414999ef7ae21167c1d02
Dawid <50092654@belfastmet.ac.uk>
2024-10-17 16:25:41 +0100

    commit

---
+++ week5_lab/string_operations.py
@@ -0,0 +1,25 @@
+# TASK ONE
+name = "Dawid Stanek"
+first_char = name[0]  #D
+last_char = name[11]
+print(first_char,last_char)
+
+
+# TASK TWO - Given a sentence "Learning Python is fun!", use slicing to extract the
+# word "Python" and print it.
+sentence = "Leaning Python Is Fun"
+word = sentence[7:14]
+print(word)
+
+# TASK THREE - Concatenate your first and last name using the + operator and repeat it
+# 3 times using the * operator.
+hello = "Dawid " + "Stanek"
+print(hello)
+
+# TASK FOUR - Experiment with at least three different string methods on your full
+# name (e.g., capitalize(), startswith(), endswith()).
+name = "dawid stanek"
+
+
+
+
